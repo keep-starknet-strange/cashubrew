719ce686f9631c3a9933d755b7ec6f8a4708f07c
Timothée Delabrouille <34384633+tdelabro@users.noreply.github.com>
2024-10-11 16:34:18 +0200

    chore: create and fix nut06 module (#11)
    * chore: create and fix nut06 module
    
    * format and credo

--- lib/cashubrew/web/controllers/mint_controller.ex
+++ lib/cashubrew/web/controllers/mint_controller.ex
@@ -2,68 +2,11 @@ defmodule Cashubrew.Web.MintController do
   use Cashubrew.Web, :controller
   alias Cashubrew.Cashu.BlindedMessage
   alias Cashubrew.Mint
+  alias Cashubrew.Nuts.Nut06
   alias Cashubrew.Web.{Keys, KeysetResponse}
 
   def info(conn, _params) do
-    info = %{
-      name: "Cashubrew Cashu Mint",
-      pubkey: Base.encode16(Mint.get_pubkey(), case: :lower),
-      version: "Cashubrew/0.1.0",
-      description: "An Elixir implementation of Cashu Mint",
-      description_long: "A Cashu Mint implementation in Elixir.",
-      contact: [
-        %{
-          method: "twitter",
-          value: "@dimahledba"
-        },
-        %{
-          method: "nostr",
-          value: "npub1hr6v96g0phtxwys4x0tm3khawuuykz6s28uzwtj5j0zc7lunu99snw2e29"
-        }
-      ],
-      time: System.os_time(:second),
-      nuts: %{
-        "4": %{
-          methods: [
-            %{
-              method: "bolt11",
-              unit: "sat",
-              min_amount: 0,
-              max_amount: 10_000
-            }
-          ],
-          disabled: false
-        },
-        "5": %{
-          methods: [
-            %{
-              method: "bolt11",
-              unit: "sat",
-              min_amount: 100,
-              max_amount: 10_000
-            }
-          ],
-          disabled: false
-        },
-        "7": %{
-          supported: false
-        },
-        "8": %{
-          supported: false
-        },
-        "9": %{
-          supported: false
-        },
-        "10": %{
-          supported: false
-        },
-        "12": %{
-          supported: false
-        }
-      },
-      motd: "Welcome to Cashubrew!"
-    }
-
+    info = Nut06.Info.info()
     json(conn, info)
   end
 
