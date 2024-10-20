f7d03ce4c6295b2d96f66bdbe01d947e65291e54
Andres Ruiz <test@example.com>
2024-10-20 13:08:28 -0500

    format

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -25,6 +25,5 @@ defmodule Cashubrew.Test.ConnCase do
   def setup_sandbox(tags) do
     pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Cashubrew.Repo, shared: not tags[:async])
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
-
   end
 end
